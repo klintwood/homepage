91c502c13ae219a975a57b944f348e38cf11f194
Daniel Klintworth <daniel.klintworth@vbg.de>
2014-06-03 17:52:52 +0200

    unify search functions

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
 var google = "https://www.google.com/#q=#{query}&safe=off"
 var youtube = "https://www.youtube.com/results?search_query=#{query}"
+var nihongomaster = "http://www.nihongomaster.com/dictionary/search/?q=#{query}&type=j"
+
+function search(e) {
+    var query = e.value;
+    window.location = google.replace("#{query}", query);
+}
 
 var sucher = document.getElementById("sucher");
 sucher.addEventListener("keydown", function (e) {
     if (e.keyCode === 13) {  // ENTER
-        search(e);    	
+        search(this);    	
     }
     if (e.keyCode === 9) {  // TAB        
         
     }
 });
 
-function search(e) {
-    var query = document.getElementById("sucher").value;
-    window.location = google.replace("#{query}", query);
-}
-
-
 var suchers = document.getElementById("suchers");
 suchers.addEventListener("keydown", function (e) {
     if (e.keyCode === 13) {  // ENTER
-        search2(e);    	
+        search(this);    	
     }
     if (e.keyCode === 9) {  // TAB
                
     }
 });
-
-function search2(e) {
-    var query = document.getElementById("suchers").value;
-    window.location = "https://www.google.com/#q=" + query + "&safe=off";
-}
