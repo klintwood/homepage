ad1631c21d9fd866aec70c2f696ba355e541f50e
Daniel Klintworth <daniel.klintworth@vbg.de>
2014-06-06 15:43:48 +0200

    add HN frontpage items

--- script.js
+++ script.js
@@ -15,8 +15,6 @@ engines["p"] = {  "title" : "Pons", "keyword" : "p",
 
 https://www.google.de/maps/search/Osterfeldstr.+30-40/@51.1758057,10.4541194,6z/data=!3m1!4b1
 
-    
-
 var selectedEngine = engines["g"];
 
 function search(caller) {
@@ -53,3 +51,112 @@ suchers.addEventListener("keydown", function (e) {
     }
     //console.log(selectedEngine["string"].replace("#{query}", this.value));
 });
+
+
+// Hacker News
+// use this instead of unreliable api: http://whateverorigin.org/
+
+function yourFunction(json) {
+    //alert(json['items'][0]['id']);
+    var items = json['items'];
+    for (var i = 0; i < 30; i++) {
+        var item = items[i];
+        var id = item['id'];
+        var title = item['title'];
+        $('ul#hacker_news').append('<li><a href="https://news.ycombinator.com/item?id=' + id + '">' + title + '</a></li>');
+    }
+}
+
+
+// http://usamadar.com/2012/06/24/getting-around-browsers-same-origin-policy-sop-with-proxies-script-injection-jsonp-and-cors/
+//httpGet('http://api.ihackernews.com/page?format=jsonp&callback=yourFunction');
+
+
+
+
+
+
+/*
+
+
+
+
+
+
+
+function httpGet(theUrl)
+{
+    var xmlHttp = null;
+
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+ function loadXMLDoc(theURL)
+    {
+        if (window.XMLHttpRequest)
+        {// code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp=new XMLHttpRequest();
+        }
+        else
+        {// code for IE6, IE5
+            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange=function()
+        {
+            if (xmlhttp.readyState==4 && xmlhttp.status==200)
+            {
+                alert(xmlhttp.responseText);
+            }
+        }
+        xmlhttp.open("GET",theURL,true);
+        xmlhttp.send();
+    }
+
+
+
+
+
+var latest_stories = httpGet('https://hn.algolia.com/api/v1/search_by_date?tags=story');
+alert(typeof JSON.parse(latest_stories['hits']));
+
+
+
+
+
+
+var frontpage = loadXMLDoc('http://whateverorigin.org/get?url=https://news.ycombinator.com/');
+var posts = frontpage.match(/https:\/\/news.ycombinator.com\/item\?id=(\d*)/);
+alert(posts);
+
+
+
+$.ajax({
+    url: "http://api.ihackernews.com/page",
+    data: null,
+    success: function(data) {
+       alert('page content: ' + data);
+    },
+    dataType: 'jsonp'
+});
+
+
+
+var request = makeHttpObject();
+request.open("GET", "http://api.ihackernews.com/page", true);
+alert(typeof request.responseText);
+
+// http://eloquentjavascript.net/chapter14.html
+function makeHttpObject() {
+  try {return new XMLHttpRequest();}
+  catch (error) {}
+  try {return new ActiveXObject("Msxml2.XMLHTTP");}
+  catch (error) {}
+  try {return new ActiveXObject("Microsoft.XMLHTTP");}
+  catch (error) {}
+
+  throw new Error("Could not create HTTP request object.");
+}
+*/
